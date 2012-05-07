3943e965f98c475a11d93b6181866cfbc34a6751
Daniel Connor <danielconnr@gmail.com>
2012-05-07 18:31:17 +0100

    testing db support

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,26 @@
-function getConfig() {
-  return {data:config};
-}
+"use strict";
+
+
+module.exports = {
+
+  testDb: function(params, cb) {
+
+    $fh.db({
+      act: "create",
+      type: "timetable",
+      fields: {
+        days: {
+          monday: "",
+          tuesday: "",
+          wednesday: "",
+          thursday: "",
+          fri: ""
+        }
+      }
+    }, cb);
+
+  }
+
+
+
+};
